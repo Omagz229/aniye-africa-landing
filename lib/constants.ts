1981fb4bf01fc8def4a8503aceb4a87363af97ea
Temituokpe Omagbemi <temituokpeomagbemi@Temituokpes-MacBook-Pro.local>
2026-06-24 12:24:08 +0100

    feat: add brand tokens, fonts, constants, robots

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const WHATSAPP_URL =
+  "https://wa.me/2348074827676?text=Hi%20Aniy%C3%A9%20Africa%2C%0A%0AI%27d%20like%20to%20send%20a%20gift.%0A%0ARecipient%20Country%3A%0AOccasion%3A%0ABudget%3A%0APreferred%20Delivery%20Date%3A";
+
+export const WHATSAPP_NUMBER = "+234 807 482 7676";
+export const EMAIL = "helloaniyeafrica@gmail.com";
